076d7a176f7884e6eb48b1611400598f4ec084a5
Dilshit <dilshikk@mail.ru>
2026-08-19 05:13:57 +0500

    feat: wrap app in LanguageProvider for global lang state

--- src/components/providers/default.tsx
+++ src/components/providers/default.tsx
@@ -1,11 +1,14 @@
 import type { ReactNode } from "react";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
+import { LanguageProvider } from "./language-provider.tsx";
 
 export function DefaultProviders({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
+      <LanguageProvider>
+        {children}
+      </LanguageProvider>
       <Toaster richColors position="top-center" />
     </ThemeProvider>
   );
